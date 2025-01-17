d01c26c0590e0ff338603b62f4b5f0581e9e5667
Tim Emiola <adetokunbo@users.noreply.github.com>
2025-01-17 19:53:24 +0900

    Update the haskell.nix pin and use GHC 9.6

--- dev.nix
+++ dev.nix
@@ -1,14 +1,14 @@
 {
-  # Pin haskell-nix to a recent (as of 2024/05/04) commit
-  h8x-commit = "5297ad9e688b8a6d1ae0e8297f5502bccb5511a5";
+  # Pin haskell-nix to a recent (as of 2024/12/29) commit
+  h8x-commit = "d65cc46ed1b8efadfbff6d41403b465525ffc225";
 
   # Pin hackage-nix to the potentially more recent version of hackage
-  hackage-nix-commit = "25618a5293d11ef32e37bc10070783c3f720fa3a";
+  hackage-nix-commit = "88fb58dc1ccce1290b7db5773a867169374b539d";
 
   # Specify the GHC version to use.
-  compiler-nix-name = "ghc928";
+  compiler-nix-name = "ghc96";
 
   # Specify the hackage index state which should be supported by either
   # the h8x-commit, or the hackage-nix-commit, whichever is latest
-  index-state = "2024-05-12T00:00:00Z";
+  index-state = "2024-12-29T00:00:00Z";
 }
